ee58334c275b56dd3d1c9783f0507566be50bca8
Christophe Van de Poel <Christophe.vdp@gmail.com>
2017-04-02 00:31:51 +0200

    remove trailing /

--- wp-content/themes/roekeloze-landing/functions.php
+++ wp-content/themes/roekeloze-landing/functions.php
@@ -3,7 +3,7 @@ function add_cors_http_header(){
 
 $http_origin = $_SERVER['HTTP_ORIGIN'];
 
-if ($http_origin == "http://localhost:4200" || $http_origin == "http://roekeloos.be/")
+if ($http_origin == "http://localhost:4200" || $http_origin == "http://roekeloos.be")
     {  
         header("Access-Control-Allow-Origin: $http_origin");
     }
