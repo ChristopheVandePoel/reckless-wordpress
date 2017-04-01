63fedd6aecb5a8815467413452d13eba700d0f0b
Christophe Van de Poel <Christophe.vdp@gmail.com>
2017-04-02 00:28:13 +0200

    allow access?

--- wp-content/themes/roekeloze-landing/functions.php
+++ wp-content/themes/roekeloze-landing/functions.php
@@ -1,3 +1,11 @@
 <?php 
+function add_cors_http_header(){
 
-// nothing here, yet (?)
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+
+if ($http_origin == "http://localhost:4200" || $http_origin == "http://roekeloos.be/")
+    {  
+        header("Access-Control-Allow-Origin: $http_origin");
+    }
+}
+add_action('init','add_cors_http_header');
